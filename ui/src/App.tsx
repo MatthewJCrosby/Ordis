8f91dcb6eb8c06427700bf1ff67413552396ec11
Matthew Crosby <crosby.matthew.j@gmail.com>
2025-07-05 16:32:25 -0700

    adjust react settings

---
+++ ui/src/App.tsx
@@ -0,0 +1,12 @@
+// ui/src/App.tsx
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Hello from React!</h1>
+    </div>
+  );
+}
+
+export default App;
