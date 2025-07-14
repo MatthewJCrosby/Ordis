5cee2dfdbac95575cea78fd106f0e3d41307d72d
Matthew Crosby <crosby.matthew.j@gmail.com>
2025-07-13 18:40:55 -0700

    'setup-flask-migrations'

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,4 +5,5 @@ class Customer(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
     
