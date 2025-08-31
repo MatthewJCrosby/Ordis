84b29e9985db57ea1a3bf4ee0fc5449b39297990
Matthew Crosby <crosby.matthew.j@gmail.com>
2025-08-31 08:57:33 -0700

    correct-line-item-logic

--- backend/app/gql/line_item/mutations.py
+++ backend/app/gql/line_item/mutations.py
@@ -2,6 +2,7 @@ from flask import g
 from sqlalchemy import select   
 from sqlalchemy.exc import IntegrityError
 from app.models.line_item import LineItem
+from app.models.product import Product
 from .. import schema
 import magql
 
@@ -18,7 +19,6 @@ LineItemCreateInput = magql.InputObject(
 LineItemUpdateInput = magql.InputObject(
     "LineItemUpdateInput",
     fields={
-        "id": "ID!",
         "product_id": "ID",
         "qty": "Int",
         "order_id": "ID",
@@ -31,12 +31,20 @@ schema.add_type(LineItemUpdateInput)
 @schema.mutation.field("createLineItem", "LineItem", args={"input":"LineItemCreateInput!"})
 def resolve_create_line_item(parent, info, **kwargs):
     input_data = kwargs["input"]
+    product = g.db.execute(select(Product).where(Product.id == input_data["product_id"])).scalar_one_or_none()
+    if not product:
+        raise Exception("Product not found")
+
     line_item = LineItem(
-        product_id=input_data["product_id"],
+        name = product.name,
+        description = product.description,
+        product_id=product.id,
+        price=product.price,
         qty=input_data["qty"],
         order_id=input_data["order_id"],
     )
     g.db.add(line_item)
+    line_item.order.update_total()
 
     try:
         g.db.flush()
@@ -58,8 +66,8 @@ def resolve_update_line_item(parent, info, **kwargs):
         line_item.product_id = input_data["product_id"]
     if input_data.get("qty"):
         line_item.qty = input_data["qty"]
-    if input_data.get("order_id"):
-        line_item.order_id = input_data["order_id"]
+
+    line_item.order.update_total()
     try:
         g.db.flush()
         g.db.commit()
@@ -74,7 +82,9 @@ def resolve_delete_line_item(parent, info, **kwargs):
     line_item = g.db.execute(select(LineItem).where(LineItem.id == line_item_id)).scalar_one_or_none()
     if not line_item:
         raise Exception("Line item not found")
+    order = line_item.order
     g.db.delete(line_item)
+    order.update_total()
     try:
         g.db.flush()
         g.db.commit()
--- backend/app/models/line_item.py
+++ backend/app/models/line_item.py
@@ -14,9 +14,14 @@ class LineItem(Base):
     name: Mapped[str] = mapped_column(String(120), nullable=False)
     description: Mapped[str] = mapped_column(String(500), nullable=True)
     price: Mapped[Decimal] = mapped_column(Numeric(10,2), nullable=False)
+    qty: Mapped[int] = mapped_column(Integer, nullable=False, default=1)
 
     product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False, index=True)
     product = relationship("Product", back_populates="line_items", lazy="selectin")
 
     order_id: Mapped[int] = mapped_column(ForeignKey("orders.id", ondelete="CASCADE"), nullable=False)
     order = relationship("Order", back_populates="line_items", lazy="selectin")
+
+
+def calculate_line_item_total(self) -> Decimal:
+    return self.price * self.qty
